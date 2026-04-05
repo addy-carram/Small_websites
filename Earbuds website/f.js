604a4b3a4fc069ec4425cadbf61b7e07bb20a01a
ady <addycarram@gmail.com>
2026-04-05 22:47:24 +0300

    the section gallery which is in the procces of making the 3D carusel effect

---
+++ Earbuds website/f.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
